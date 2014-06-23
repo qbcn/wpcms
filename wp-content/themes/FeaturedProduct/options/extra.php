0644001d1e39ba0c09d2e36c701b4686c626c6eb
ronghui <ronghui.hz@gmail.com>
2014-06-23 19:41:47 +0800

    crack

--- wp-content/themes/FeaturedProduct/options/extra.php
+++ wp-content/themes/FeaturedProduct/options/extra.php
@@ -1,3 +1,134 @@
-<?php // This file is protected by copyright law & provided under license. Copyright(C) 2008-2020 www.wpyou.com, All rights reserved.
-$OOO0O0O00=__FILE__;$OOO000000=urldecode('%74%68%36%73%62%65%68%71%6c%61%34%63%6f%5f%73%61%64%66%70%6e%72');$OO00O0000=8108;$OOO0000O0=$OOO000000{4}.$OOO000000{9}.$OOO000000{3}.$OOO000000{5};$OOO0000O0.=$OOO000000{2}.$OOO000000{10}.$OOO000000{13}.$OOO000000{16};$OOO0000O0.=$OOO0000O0{3}.$OOO000000{11}.$OOO000000{12}.$OOO0000O0{7}.$OOO000000{5};$O0O0000O0='OOO0000O0';eval(($$O0O0000O0('JE9PME9PMDAwMD0kT09PMDAwMDAwezE3fS4kT09PMDAwMDAwezEyfS4kT09PMDAwMDAwezE4fS4kT09PMDAwMDAwezV9LiRPT08wMDAwMDB7MTl9O2lmKCEwKSRPMDAwTzBPMDA9JE9PME9PMDAwMCgkT09PME8wTzAwLCdyYicpOyRPTzBPTzAwME89JE9PTzAwMDAwMHsxN30uJE9PTzAwMDAwMHsyMH0uJE9PTzAwMDAwMHs1fS4kT09PMDAwMDAwezl9LiRPT08wMDAwMDB7MTZ9OyRPTzBPTzAwTzA9JE9PTzAwMDAwMHsxNH0uJE9PTzAwMDAwMHswfS4kT09PMDAwMDAwezIwfS4kT09PMDAwMDAwezB9LiRPT08wMDAwMDB7MjB9OyRPTzBPTzAwME8oJE8wMDBPME8wMCwxMjYxKTskT08wME8wME8wPSgkT09PMDAwME8wKCRPTzBPTzAwTzAoJE9PME9PMDAwTygkTzAwME8wTzAwLDE4NDQpLCd4UEducnpPTCtncTc0OVQzWlJ3SEtrY3VwNnY1YThXeWpCSWRDaG8xbU1pWDBiZS9TVmx0QXMyREVZUVVGSmZOPScsJ0FCQ0RFRkdISUpLTE1OT1BRUlNUVVZXWFlaYWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXowMTIzNDU2Nzg5Ky8nKSkpO2V2YWwoJE9PMDBPMDBPMCk7')));return;?>
-82BM5OKmqGBMaD9h8GjCwzRKKzJHRkgcRkgykCzwKs01KAkwkCkwuAYPHKK1uwCMgIpm+ckl6c8MqGamqGEiuGEM328B52VB5o8dvOkbuGYd52AMyGjmuGEiuzSeqHJ0529B5OB/aDZMyGjmuGEiuzSeqHFV4d8a7dPa7dxMyGjmuGEiuzSeqHFVTHga7dr2TzSe4whFqGBa7IMauGEM3D8SWcJsuGYd52AMyGjmuGEiuzSeqHJDaLP/azSep2JbquSmqzSeqhVa7ICN6LkBaLPa7o9/5whFqGBa7IMauGEM3D9M5ozBaLPa7o9/5whFqGBa7IMauGEM32sY4D8a7o9/5whFqGBa7IMauGEM329mvcYB8tRa7o9/5whFqGBa7IMauGEM329mvcYB8t9a7o9/5whFqGBa7IMauGEM329mvcYB8tga7o9/5whFqGBa7IMauGEM329mvcYB8tza7o9/5whFqGBa7IMauGEM328/8OJManza7o9/5whFqGBa7IMauGEM328/8OJManga7o9/5whFqGBa7IMauGEM328/8OJMan9a7o9/5whFqGBa7IMauGEM328/8OJManRa7o9/5whFqGBa7IMauGEM328/8OJManka7o9/5wC17GR+kzRZus9zKh6zKhJcZkgHcl8HRkgcRkgyHCz9Rw88qwCMyLSmqOhta2kAqGRyKAkwkCkwcl8+kzRZuAB3KsZ1uwCMgIpm+ckl6c8MqGamqGEiuGEM328B52VB5o8dvOkbuGYd52AMyGjmuGEiuzSeqHJ0529B5OB/aDZMyGjmuGEiuzSeqHFV4d8a7dPa7dxMyGjmuGEiuzSeqHFVTHga7dr2TzSe4whFqGBa7IMauGEM3D8SWcJsuGYd52AMyGjmuGEiuzSeqHJDaLP/azSep2JbquSmqzSeqhVa7ICN6LkBaLPa7o9/5whFqGBa7IMauGEM3D9M5ozBaLPa7o9/5whFqGBa7IMauGEM32sY4D8a7o9/5whFqGBa7IMauGEM329mvcYB8tRa7o9/5whFqGBa7IMauGEM329mvcYB8t9a7o9/5whFqGBa7IMauGEM329mvcYB8tga7o9/5whFqGBa7IMauGEM329mvcYB8tza7o9/5whFqGBa7IMauGEM328/8OJManza7o9/5whFqGBa7IMauGEM328/8OJManga7o9/5whFqGBa7IMauGEM328/8OJMan9a7o9/5whFqGBa7IMauGEM328/8OJManRa7o9/5whFqGBa7IMauGEM328/8OJManka7o9/5wC17GRyKAkwkCkwcl8+kzRZuAB3KsZ1uwCMqwhCvcKmgFHQlX1HSUure9fFbb5DtT7ceFHas9Xce7vm+O8B52VB5o8dvOkb7o9/5wn9f9cftF7KlJnZ74d1ATd+i4NWly2Fb0yXSvX3bkzR/F2DNdmjTnZVTHjE9dx1qH0CHAFS4rFS4rFS3u9AahJl6uP0pc9hqG8yuA6gHrkyula0+IaI7IR3HAFSHtP34nxe+IaI7GjCHAJ34nxS4rFSqGR3HtP3HtxSHtxmgrJ34rJ34nxSHljCHtxS4rFSHtxS7GR3HtxSHtxS4nxM7G8EKr8ea1M3HGb1aHaATkZtchgDwrbXpDkS91pspHBuWcMGwcRnvOFV5KsMcnPI6wJHkoVAZu4lRrk6KkkOwo6T3wa0gAzGZARzRC8+wKM7HrsTHsPRKh9Kkk6uczhvpcgd6Oko62BMvob05cY/aLzlaDRs818EWumS4H+t9nK29tjYqlF1qwCMqHbop2V/a2KmgrFS4nP34rFS4GCU6u6B5GjCHAFS4rFS4rFSqH0=nZMo8cYd8Oh/5IPdpuRdvzJS5D9Au2hbpc8hqGCjWSAq62V/poz0+GRS5D9A7GRS5D9Aat09GIRovugt8zJM5caj3wx1gt09GoJIuD9ApugAqGCUnZM/phJh5oRyp2VhpcEmqH09GIR/8uRS8uZj3wPSaok1u2sB8O9mu2z05Gj17tVM5caeqD9lpts5uGaIuwB5uhS1+hAXqkbaglg87Imf72C17GRS5D9A7HYS5D9Au29/51Rh51Z0gOsB8O9m6u4MTSAqgO6Ma19Au2hb6lxJ+GRbpuRdvOkt+z0VuwP54zAUnZMM6IBh5uPAWwjC6ohlaDRyvcs1qwhUnZmCa2hA6kJsaoSj3wPI5OJ1vcYo5lj18OkbaOVB8Oky8ug0glCUnZmC6ohlaDRyvcs1+nAj+IRtvuRhuDkl5GJM5cz16u4/5oFb8OBs5cgepch07oMS6l+UnZMJnZMl6uRsaoEjgO6Ma19Au2hb6t09G1A9Go6s5o9AvcJe+O9B8O9muD90vcRhahJM5cz16wjM+L09Go8052gB5GxCaOJt8GSCaOJt8L4UnZmC6ohlaDRyvcs1+nAjglaUnZM/phJt8Ozl8GjMTSAq52gy6cYCu2906czeqGCUnZmC5DkAaLkA+nAjaLgh6sJbpuRdvzJB5OSmglFFvcs17Ibtao4JcsS1+hAmcsYaglg8qlh5uGaIuwEi3IJMglSCaOJt8GAfaOJt8zJd52YA6cYA7GRbpuRdvOktqH09GIRovugt8zJM5caj3wxC5czAp2BhalP54kAjctP8TSAqvcpm6csS8LCmgO6Ma19Au2hb6lCMWSAqgL9M8Oky8ug0+nAjpoV/62he6oFmgDRh5uP0puRhuDkl5GaMTSAqgO6Ma19Au2hb6lxJ+G+Ca2hA6kJsaoS/vcsB62kt72Y/7u90vcRhaIYiaOaITSAqyZAqaokA8uge+GRovugt8zJM5caUnZMJnZMo8cYd8Oh/5IP16uRyp2zA6c8/a1hyaoJ/8zJM6GjCp2zAqZAqWSAqgLRmvu9yp2zA6c8/a1Cj3wP16uRyp2zA6c8/a1CmgO9B8GCUnZMDvOh06wjC8OBMasJdpuRh62JlWwAfp2zA6c8/a1hyaOzl6cYAqZAqWSAqgLRmvu9yp2zA6c8/a1Cj3wP16uRyp2zA6c8/a1CmgLRmvu9yp2zA6c8/a1Cb3o9B8Ok15DgYuDPBaoke8GCUnZMJnZMl6uRsaoEjgLRmvu9yp2zA6c8/a1Cb31RhaosyvcZUnZMJnZMo8cYd8Oh/5IPS5D9Au2htu2heu2Rha29h5oRB51Ryp2zA6c8/a1Cm+GRdpuRt7GRyaOJt8GxJ+OYs5OSjqZAqWSAq6oJl6czdvGxm+GBBa1gBWwCjgO9B8L4jpu4jgO9B8GxM+L09GIRC6u9d6cYCpcYAalxJ+O8h8zJA6ugbu29mvcVCaokeqGxmvcYAqwxCp2zA7G8dpuRh62JlWwaMTSAqvcpjqGxC6Oktp2ke6Oze8L4jgI6M5hJdpuRh62JlWwjjgORha29h5oRB51Rt7GRyaOJt8GxM+GC9G1gh8Lkl5IPAa1khTSAqyZAqaokA8uge+O6B5L9hTSAqyZAq61kepDRM52Ej8DPY5DkyaOJt8zJAvLkbpoYBvcSm+GRDvcRAvGxJ+nrS4GSCvOkM62BA+nAj4HxS+GhUnZM15OJIpcSjgLP/aDZUnZmCa2hA6kJsaoSj3wP16uRypoV/62he6oFmgDRh5uP0puRhuDkl5GaMTSAqvcpjqGP16uRyaOJt8zJb6uRBqGRS5D9A7HYgRGS18OBs5cgepch0glVAa1khqwxM+L0C8OBs5cgepch0uD9lplxJ+O8h8zJS5D9Au2sh8OrmgLP/aDZb3Chr7G8AvLkbpoYBvcS17LRl8cKMTDA9Gok0a2kM6IjjvOztuDP/aDRy8OBs5cgepch0qGCjqwPUgLRm8csI5ozM5zJtao4j3wPDazJ16uRypuRApc9m5cke8zJM5cz16kJtao4m62kAuDP/aDRy8OBs5cgepch0u2hCqGRS5D9A7HYgRGC0puglpuCm+GRDvcRAvGSCvOkM62BA+GC06oz0a2KMTlRAvLkbpoYBvcVyaDgd+nAjgLRm8csI5ozM5zJtao954zAUyZAq6cVt6wPUnZmC8OBs5cgepch0uD9lplxJ+Ga1TSAq52gyaDRBa1ZmqH09GoJIu2ke6zJd5OkB5IjMTSAqgOJs8LPs8GxJ+LPl6c8y5czAp2BypcV0qGa/3Ohb6lEXaDgd3kbaglg8qzbWuGaIuw0McsS1+hAeqdE/vwa0gLP/aDZb31P/aDRyp2Je8Oke8GSC5czAp2BhalCUnZmC8OBs5cgepch0uD9lplxJ+GRbpuRdvOkt+z0VuwP54zAUnZMM6Ijj+ckbaLRYqGRAvLkbpoYBvcVyaDgdqwxMWSAqgLRm8csI5ozM5zJtao4j3wxC5czAp2BhalP54kAjctP8TSAqyck0a2KjWlRAvLkbpoYBvcVyaDgd+nAj+IRtvuRhuDkl5GJM5cz16u4/5oFb8OBs5cgepch07oMS6l+UyZAqyZAq6c9m5lxCaOJt8zJAvcsAvLkbpIxJ+GaFvcs1+L9lptAIglEC8OBs5cgepch0uD9lplE1+IPB5LZJ+IaegLP/aDZb31P/aDRy8OhA5OKegl+jp2VBaD4J+1Rm8csI+Ix/3IaUnZMJnZMo8cYd8Oh/5IPDaLh/8kJt8LgM5u8M6LRmqGRt8L+j7GRt8Ozl8Gx0gL8M6LRm+GSC8LgM5csBaobhaIxMWSAqgOJs8LPs8GxJ+LPl6c8yaokS5Ozd6wj17sEm3tM5uLjS4GsaWn8OuuV5uLBn4GsaWr6OukbaWnjS7kVEZC68qlhU4GS17IRt8Ozl8GE1ywjm3tM5uLjS4GsaWn8OuuV5uLBn4GsaWr6OukbaWnjS7kVEZC68qlhU4GS17IRDvcRAvGE1ywCeqIJtglS1unr17GRt8L+MTSAqaokA8uge+GR/8uRS8uZegLRlvcsbpugX6u+UnZMJnZMo8cYd8Oh/5IPDaLh/8kJSpc8h5oz2vwjCaoze62Kj3wxYqu09Go8052gB5GxCaOz16cZ0gL8SuDzs6ugYTSAqvcpjqGxBgOsBWzJSpc8h+GCjWlRbpuByaOz16wxJ+GRDazJV8cklWwAf5czEu2Ys5kJSpc8hatbJnZMM6IjC5czEuDPB62Kj3drMW2hoqGrCaOz16cZMWlRSpc8h6GxJ+nrUyZAqaLgh8oh/8u9yaOJt8L9y5Ohevlj1Y7IqY7IxQvOsglCUnZMM6IjC5czEuDPB62Kj3IRlpcY16whUnZMM6IjCaOz16cZj3GRlpcY16whU6oJlqGRM+nAj4H0CvwxF3wxmgLgB5o8h+G0VqH0Cvw0Xqubhp2B/+G+FpwPmaoko3waI7o8h8zJSpc8h51kbu2VM5o0mgOCM+GEIgl+UnZMM6IjCvHAJgLPB62kCqckdvOFj+IPd5OztatA1pDklaoke8GaIT2kdvOFj+dECvHS/pHEITDsJnZMh5L9hvcpmgLPB62kC+nEJ+GjC5czEuDPB62Kj7c9hvcSmqGRlpcY16wFlqwCMqu09Go6/aIjCvwxJ+GRbpuByaOz16wxbgLgB5o8hTlRM+nSJ+GRbpuByaOz16H0Cvw0Xqubhp2B/+G+FpwPmaoko3waI7o8h8zJSpc8h51kbu2VM5o0mgOCM+GEIgl+UnZMM6IjCvHAJgLPB62kCqckdvOFj+IPd5OztatA1pDklaoke8GaIT2kdvOFj+dECvHS/pHEITDsJnZMh5L9hvcpmgLPB62kC+nEJ+GRlpcY16wxogIRSpc8h6GxFqGRbpuByaOz16wxbp2kM5GjmgLgB5o8h7t+MqwCMWSAq6oJlqGRM+nAjqGRSpc8h6Gxbp2kM5GjCaoze62K/4ICMTlRM+nSJ+GjCaOz16cZjq29hvcSmqGRlpcY16wFlqwCMTlRMql0MW2kdvOFj+dVB+OBl6cpJgl+e62kAuDPB62ke8csy5OhevljCvwCj7I+1+dbM6IjCvHAJgLPB62kCqwPhp2B/+G+jp2VBaD4Jg29sa1gh51Z1+dbhp2B/+G+fgOCF72rf+dbJyuA9Gok0a2kU6oJlqGRM+nAj4H0CvwxF3wxC5czEuDPB62KUgOCXqlhU6c9m5lxI3OrjvLgh6dA1+IY16uRyaOz16cYs5kJ0vcYXqGRMqwxe+IaITSAqvcpmgOCJ3wRSpc8h6Ghhp2B/+G+jp2VBaD4Jg29sa1gh51Z1+dbhp2B/+G+fgOCF72rf+dbJyZAq5okE8zJS5D9AasJ0vcYXqGyCe+/Ce+nMm5K1qH09G1A9G1A9Go90pu9t+L8SWcJsuD8M6O8h8zJHaOkdvcz0Z2zAKOJt8L4j6uBA6cYCalPuKzJuvcR16uZjWSAq61kepDRM52Ej8DPY5Dky82hC62kAus9S6c9MpcVnpuRZ5D9AaljM+L09GIRDvcR16uRy5DPt+nAjpuglpuCmg290pu9t5ozb6waJ3I8DaLh/8kJDvcR16uRyKDPhp2hB5r9B8zP/aDRtglS16OktpDgMaLRM52E13HYyuljj+evp/eWCeev4Bfceoec+BeWVefWvBTvcBfWXmTc+hfIBiG+M+GCUnZmC8OBMalAfksPyk2hC62kAqG8HaOkdvcz0Z2zAKOJt8L417zJyqG8uKzh3kWv4Bfceoec+BeWVefvcBfWXmGaM7GRDvcR16uRy5DPtqH09G1A9Go6s5o9AvcJe+L8M6O8h8GjCpug1alSCvcYt8Ozep2KM+L09GokE8LgBpDZmgOzl6D4MTSAqgOzl6D95gDP/aDRd5Dke8L41uwxJ+OkbaLRYqGRM519ApcYd6k01aOJt8O9/8cYAal88qwxNgtrSgtmjgOheaDRB5o9hcl8S5D9Ap2Js51RtgsAUnZmCpug1as01p2zAvcZ1uwxJ+OkbaLRYqGRM519ApcYd6k01p2zAvcZ1uwCj3laVgtmjgOheaDRB5o9hcl8dpuRM6G88TSAqKDPhp2hB5r9B8zP/aDRtqGRBao8tqH09G1A9Go6s5o9AvcJe+LkS6OzA6wjC5okDu2heaDRB5o9h7GR/5ORyvcYt8Ozep2KM+L09GIRM519ApcYd6wxJ+GR/5ORyvcYt8Ozep2KUnZmCvcYt8Ozep2k5gDP/aDRd5Dke8L41uwxJ+L9AaohSuDRB6D4maDRlvuPt5OztvOktqGRe6u8yvcYt8Ozep2k5gDP/aDRd5Dke8L41uwCMTSAqgOheaDRB5o9hcl8dpuRM6G88+nAjaDRlvuPy8Oz1alBt8LgMaL90pu9m6u4mgOYh8sJM519ApcYd6k01p2zAvcZ1uwCMTSAqaokA8uge+GRe6u8yvcYt8Ozep2KUnZMJnZMo8cYd8Oh/5IPo5DgbqGRM519ApcYd6wCjWSAqgOheaDRB5o9h+nAj8DPyaOzla2kypug1aljjqOzlaozYqwxCvcYt8Ozep2K0puglpuCmgDRM8OVhgtAfgla0gDP/aDRd5Dke8L413HE14Hx1qwxMTSAqgLP/aDRd5Dke8L4j3wPm8Os0aDPhp2hB5O9mpugtqGRM519ApcYd6k01aOJt8O9/8cYAal88qH09GIRdpuRM6GxJ+OBA5cVtaOkdvcz0p2BBa14mgOheaDRB5o9hcl8dpuRM6G88qH09Gdbhp2B/+Gaj+Gxj+Gxj+nVS3jAq+Gxj+Gxj+Gxg3OVBpok0+O6/adAIgtbhp2B/+GRAvOht7HY16uRy6ohh5ORyvcZmg29B8OhCglCUT2kdvOFjgl+fgtby6wj1YmlLYvQvYpIOYUOUwKZQglCUT2kdvOFjgtS/5OzI6cSfnZmj+Gxj+Gxj+xCFa2k06c9A+OYB5cKJ+IaU6c9m5lxC8OBMalAf62kAu26M6cVCu2YB5cKmg29B8OhCglCUT2kdvOFjgl+jvcZJ+IaU6c9m5lxC8OBMalAf62kAu26M6cVCu2hCqG8dpuRM6GaMTtbhp2B/+GaI+O90pu9t3wgS5D9A6oJl5w+fnZmgGZCggt09GIRdpuRh62Jlvckt+nAj62kAu29B8Ok15DgM6u4mg2BM6Oky6csS8LCJ4G6/aoRhaogY3chCglCUnZmC8DPyp2zAalxJ+OzlaozYqGCUnZMo5Dghpc9m+GjCp2zA6c8/aohhalPBalxCp2zA6c8/a1hy5Oht8GxM+L09GIRDazJdpuRtclRdpuRh62JlWkJ0vu9A7HYdpuRywKR8+nAjgO9B8Ok15DgYu2VMaDZb3o9B8zJepcshTSAqT2kdvOFjglxj+Gxj+Gxj+Gxj+Gxj+Gxj+Gxj3OJS8Oh/5IP2pcVs6HAIgtbhp2B/+GRdpuRh62JlWkJ0vu9A7HYdpuRywKZUT2kdvOFjgl+jgtbM6IxmgO9B8OhC+nAJ+GRdpuRh62JlWkJ0vu9A7HYdpuRywKZM+GPhp2B/+G8t6cVhpDRh6nAIa2k06c9A6cZIgt0U6c9m5lx13IaU6c9m5lxC8DPyp2zAas0Cp2zA6c8/a1hy5Oht8GAfp2zAuAhruH0U6c9m5lx13GJ/aLRM52EfnZmj+Gxj+Gxj+Gxj+Gxj+GxjgtbJT2kdvOFjglxj+Gxj+Gxj+Gxj+nS/a2k06c9A3jAq+Gxj+Gxj+GxF7DxfnZmj+Gxj+Gxj+nVS3dV0pcgh5GPo5D+J+IaU6c9m5lxC8OBMalAf62kAu26M6cVCu2hCqG8S5D9Ap2Js51RtglCUT2kdvOFjgl+fgtby6wj1YMIfYQwQYMvLYQejYMcSQpW3TIaMTtbhp2B/+GaF72VBpok03IxFvcYS8uZja2hQ6HAI4l+jvcZJ+IaU6c9m5lxC8OBMalAf62kAu26M6cVCu2hCqG8S5D9Ap2Js51RtglCUT2kdvOFjgl+j5ozb6HAIgtbhp2B/+GRAvOht7HY16uRy6ohh5ORy5ozb6wj1aOJt8O9/8cYAalaMTtbhp2B/+GaI+LRYaOKJ+1RhWLZI+L6B5Lkh3w+1T2kdvOFjgLP/aDRd5Dke8L4UT2kdvOFjgl+j7tEF7DxfnZm1TSAqyZAqyZAq61kepDRM52EjKDPhp2hB5r9B8zP/aDRtqGRBao8t+nAjpuglpuCmqwCjWSAq62V/poz0+GRDaORITSAqgLP/aDRd5Dke8L4j3wxCpug1as01aOJt8O9/8cYAal88TSAqgO9B8OhC+nAjgOzl6D95g29B8OhCgsAUnZMhp2B/+GRBao8tcl8I6c6/aoky82hC62kAgsAUnZmU6c9m5lx1GHVm4tEFpwPmaoko3w+1T2kdvOFj62kAu29B8Ok15DgYu2VM5o0mgO9B8OhCqH0U6c9m5lx1+IPAvuR06HAIgtbhp2B/+O8h8zJdpuRy5ozb6wjjgO9B8OhC+GCUT2kdvOFjgl+fgtbhp2B/+O8h8zJdpuRy5ozb6wjjgO9B8OhC+GCUT2kdvOFjgtS/pHEF72jt3jAq+Gxj+nVs5nE9GjCggt09Go8052gB5GxCaOJt8n09GIRl6c9h51RS5D9AalxJ+O8h8zJS5D9Aalj1p2zA6c8/a1CJglECp2zAvcZj7Iao51kbpoklaOJt8L4JglECaOJt8O9/8cYAalCUnZMo5Dghpc9mqGRl6c9h51RS5D9AalPBalxCaOJt8GCjTjAqT2kdvOFjglxj+Gxj+Gxj+Gxj+nV0vHEFpwPmaoko3w+1TDRm6kJS6ugbpcVM5o0mqH0U6c9m5lx1+IPAvuR06HAIgtbAvOky8OhA5OKmqH0U6c9m5lx1+dE1TDRm6kJAvuR06wjMTtbhp2B/+GaF72rf3GJ0vHE9GIxj+Gxj+Gxj+GaU6cYC6oJl6czdvn0U6c9m5lx1+Gxj+nS/8cSf+xAqgtbhp2B/+GRBao8tcl8B61RhahJDvcR16uZ1uH0U6c9m5lx1gtbJT2kdvOFjglaUaok1vu9A6ugy82hC62kAqG8DaLh/8kJDvcR16uRyKDPhp2hB5r9B8zP/aDRtglCU
+<?php 
+function catch_post_image() {
+global $post,$posts;
+$first_img = '';
+ob_start();
+ob_end_clean();
+$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i',$post->post_content,$matches);
+$first_img = $matches [1] [0];
+if(empty($first_img)){
+$site_url = bloginfo('template_url');
+$first_img = "$site_url/images/no-thumbnail.jpg";
+}
+return $first_img;
+}
+function catch_slider_image() {
+global $post,$posts;
+$first_img = '';
+ob_start();
+ob_end_clean();
+$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i',$post->post_content,$matches);
+$first_img = $matches [1] [0];
+if(empty($first_img)){
+$site_url = bloginfo('template_url');
+$first_img = "$site_url/images/no-slider.jpg";
+}
+return $first_img;
+}
+function get_category_root_id($cat)
+{
+$this_category = get_category($cat);
+while($this_category->category_parent)
+{
+$this_category = get_category($this_category->category_parent);
+}
+return $this_category->term_id;
+}
+function post_is_in_descendant_category( $cats,$_post = null )
+{
+foreach ( (array) $cats as $cat ) {
+$descendants = get_term_children( (int) $cat,'category');
+if ( $descendants &&in_category( $descendants,$_post ) )
+return true;
+}
+return false;
+}
+function wpyou_post_thumbnail( $width = 100,$height = 100 ){
+global $post;
+$site_url = get_bloginfo('template_url');
+if ( get_post_meta($post->ID,'thumbnail',true) ) {$thumbnail_src = get_post_meta($post->ID,'thumbnail',true);}
+elseif( has_post_thumbnail() ) {$thumbnail_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),array( $width,$height ),false);$thumbnail_src = $thumbnail_src[0];}
+else {
+$thumbnail_src = '';
+ob_start();
+ob_end_clean();
+$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i',$post->post_content,$matches);
+$thumbnail_src = $matches [1] [0];
+if( !empty($thumbnail_src) ){
+$thumbnail_src = $matches [1] [0];
+}else {$thumbnail_src = "$site_url/images/no-thumbnail.jpg";}
+}
+echo $post_timthumb = '<img src="'.$thumbnail_src.'" alt="'.$post->post_title.'" class="thumb" />';
+}
+function wpyou_strimwidth($str ,$start ,$width ,$trimmarker ){
+$output = preg_replace('/^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$start.'}((?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$width.'}).*/s','\1',$str);
+return $output.$trimmarker;
+}
+function wpyou_pagenavi($range = 9){
+global $paged,$wp_query;
+if ( !$max_page ) {$max_page = $wp_query->max_num_pages;}
+if($max_page >1){if(!$paged){$paged = 1;}
+previous_posts_link('上一页');
+if($max_page >$range){
+if($paged <$range){for($i = 1;$i <= ($range +1);$i++){echo "<a href='".get_pagenum_link($i) ."'";
+if($i==$paged)echo " class='current'";echo ">$i</a>";}}
+elseif($paged >= ($max_page -ceil(($range/2)))){
+for($i = $max_page -$range;$i <= $max_page;$i++){echo "<a href='".get_pagenum_link($i) ."'";
+if($i==$paged)echo " class='current'";echo ">$i</a>";}}
+elseif($paged >= $range &&$paged <($max_page -ceil(($range/2)))){
+for($i = ($paged -ceil($range/2));$i <= ($paged +ceil(($range/2)));$i++){echo "<a href='".get_pagenum_link($i) ."'";if($i==$paged) echo " class='current'";echo ">$i</a>";}}}
+else{for($i = 1;$i <= $max_page;$i++){echo "<a href='".get_pagenum_link($i) ."'";
+if($i==$paged)echo " class='current'";echo ">$i</a>";}}
+next_posts_link('下一页');
+}
+}
+class wpyou_widget_SpecialCatPosts extends WP_Widget {
+function wpyou_widget_SpecialCatPosts() {
+$widget_ops = array('classname'=>'wpyou_widget_SpecialCatPosts','description'=>__( "显示指定分类的文章列表") );
+$this->WP_Widget('SpecialCatPosts',__('WPYOU指定分类文章'),$widget_ops);
+}
+function widget($args,$instance) {
+extract($args);
+$args['postcounts'] = empty($instance['postcounts']) ?'10': $instance['postcounts'];
+$args['catid'] = empty($instance['catid']) ?'1': $instance['catid'];
+SpecialCatPosts($args);
+}
+function update($new_instance,$old_instance) {
+$instance = $old_instance;
+$instance['postcounts'] = strip_tags(stripslashes($new_instance['postcounts']));
+$instance['catid'] = strip_tags(stripslashes($new_instance['catid']));
+return $new_instance;
+}
+function form($instance) {
+$instance = wp_parse_args( (array) $instance,array('title'=>'','postcounts'=>'10') );
+$postcounts = htmlspecialchars($instance['postcounts']);
+$catid = htmlspecialchars($instance['catid']);
+;echo '        <p>
+        	<label for="';echo $this->get_field_id('catid');;echo '">';_e('指定分类ID:');;echo '</label>
+        	<select name="';echo $this->get_field_name('catid');;echo '" id="';echo $this->get_field_id('catid');;echo '" class="postform">
+				';
+$categories = get_categories('hide_empty=0&orderby=id');
+$wp_cats = array();
+foreach ($categories as $category_list ) {
+$wp_cats[$category_list->cat_ID] = $category_list->cat_name;
+;echo '                    <option value="';echo $category_list->cat_ID;;echo '" ';if ($catid == $category_list->cat_ID)  echo 'selected="selected"';;echo '>';echo $wp_cats[$category_list->cat_ID];;echo '</option>
+                ';};echo '            </select>
+        </p>
+        <p><label for="';echo $this->get_field_id('postcounts');;echo '">';_e('显示文章数量:');;echo '</label> <input size="3" id="';echo $this->get_field_id('postcounts');;echo '" name="';echo $this->get_field_name('postcounts');;echo '" type="text" value="';echo $postcounts;;echo '" /></p>
+';
+}
+}
+function SpecialCatPosts($args = array()) {
+global $wpdb;
+$postcounts = $args['postcounts'];
+$catid = $args['catid'];
+echo $args['before_widget'];
+;echo '	<h3><a href="';echo get_category_link($catid);;echo '" title="';echo get_cat_name( $catid );;echo '">';echo get_cat_name( $catid );;echo '</a></h3>
+    <ul>
+		';
+global $post;
+$recentposts = get_posts('category='.$catid .'&numberposts='.$postcounts);
+foreach($recentposts as $post) :
+;echo '            <li><a href="';the_permalink();;echo '" title="';the_title();;echo '">';the_title();;echo '</a></li>
+         ';endforeach;;echo '    </ul> 
+';echo $args['after_widget'];;echo '';};echo '';register_widget('wpyou_widget_SpecialCatPosts'); ?>
